e1afefbf9ac8de62b00deabeadb3aeecb7354001
Tanner Chell <tanner.chell@gmail.com>
2019-02-25 14:49:40 -0700

    added BookList Class and Tests

---
+++ app/src/main/java/ca/team21/pagepal/BookList.java
@@ -0,0 +1,15 @@
+package ca.team21.pagepal;
+
+import java.util.ArrayList;
+
+public class BookList {
+    private ArrayList<Book> bookList;
+
+    public BookList() {}
+
+    public void add(Book book) {}
+
+    public void remove(Book book) {}
+
+    public ArrayList<Book> get() {return bookList;}
+}
