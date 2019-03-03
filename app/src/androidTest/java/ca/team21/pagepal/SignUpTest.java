f39d2eb572fc1ab0933cda84b114156ae6968a93
Tanner Chell <tanner.chell@gmail.com>
2019-03-03 14:44:18 -0700

    Changed name to username in SignUpActivity
    - new users must now enter a Username instead of Name when signing up
    - usernames are checked against the database to ensure uniqueness
    - added a library for email validation
    - #32

--- app/src/androidTest/java/ca/team21/pagepal/SignUpTest.java
+++ app/src/androidTest/java/ca/team21/pagepal/SignUpTest.java
@@ -1,26 +1,12 @@
 package ca.team21.pagepal;
 
-import android.support.annotation.NonNull;
-import android.util.Log;
-import android.widget.Toast;
-
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-
-import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import java.lang.reflect.WildcardType;
-
 import androidx.test.rule.ActivityTestRule;
 import androidx.test.runner.AndroidJUnit4;
 
-import static androidx.test.espresso.Espresso.onData;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
@@ -28,7 +14,6 @@ import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.hasErrorText;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static org.hamcrest.Matchers.instanceOf;
 import static org.junit.Assert.assertNotNull;
 
 @RunWith(AndroidJUnit4.class)
@@ -86,7 +71,7 @@ public class SignUpTest {
     }
 
     private void typeInput(String name, String email, String password, String confirmPassword) {
-        onView(withId(R.id.name_input)).perform(typeText(name), closeSoftKeyboard());
+        onView(withId(R.id.username_input)).perform(typeText(name), closeSoftKeyboard());
         onView(withId(R.id.email_input)).perform(typeText(email), closeSoftKeyboard());
         onView(withId(R.id.password_input)).perform(typeText(password), closeSoftKeyboard());
         onView(withId(R.id.confirm_password_input)).perform(typeText(confirmPassword), closeSoftKeyboard());
--- app/src/main/java/ca/team21/pagepal/SignUpActivity.java
+++ app/src/main/java/ca/team21/pagepal/SignUpActivity.java
@@ -1,9 +1,16 @@
+/**
+ * sources:
+ *  - custom error icon: https://medium.com/the-wtf-files/the-mysterious-case-of-the-error-drawable-2e8e9aff805
+ */
 package ca.team21.pagepal;
 
+import android.graphics.drawable.Drawable;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
 import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -16,9 +23,14 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthUserCollisionException;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
+import org.apache.commons.validator.routines.EmailValidator;
 
 import java.util.Date;
 
@@ -27,12 +39,14 @@ public class SignUpActivity extends AppCompatActivity
 
     private static final String TAG = "SignUpActivity";
 
+    private Drawable checkmark;
+
     private FirebaseAuth mAuth;
     private FirebaseUser authUser;
     private DatabaseReference dbRef;
     private User user;
 
-    private EditText nameView;
+    private EditText usernameView;
     private EditText emailView;
     private EditText passwordView;
     private EditText confirmPasswordView;
@@ -46,14 +60,40 @@ public class SignUpActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
+        // Set up OK checkmark
+        checkmark = getResources().getDrawable(R.drawable.ic_check_circle_24px);
+        if (checkmark != null) {
+            checkmark.setBounds(0, 0,
+                    checkmark.getIntrinsicWidth(),
+                    checkmark.getIntrinsicHeight());
+        }
+
         mAuth = FirebaseAuth.getInstance();
         dbRef = FirebaseDatabase.getInstance().getReference();
 
-        nameView = findViewById(R.id.name_input);
+        usernameView = findViewById(R.id.username_input);
         emailView = findViewById(R.id.email_input);
         passwordView = findViewById(R.id.password_input);
         confirmPasswordView = findViewById(R.id.confirm_password_input);
 
+        usernameView.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (!s.toString().isEmpty()) {
+                    queryUsernames(s.toString());
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
         Button signUpButton = findViewById(R.id.sign_up_button);
         signUpButton.setOnClickListener(this);
     }
@@ -62,7 +102,7 @@ public class SignUpActivity extends AppCompatActivity
     public void onClick(View v) {
         if (v.getId() == R.id.sign_up_button) {
 
-            name = nameView.getText().toString();
+            name = usernameView.getText().toString();
             email = emailView.getText().toString();
             password = passwordView.getText().toString();
             confirmPassword = confirmPasswordView.getText().toString();
@@ -71,6 +111,34 @@ public class SignUpActivity extends AppCompatActivity
                 finish();
             }
         }
+    }
+
+    /**
+     * Check the current username against all username in the database. If the username is free, the
+     * error is set with a checkmark. If the username is taken the error is set normally.
+     *
+     * @param text The username to look for.
+     */
+    public void queryUsernames(String text) {
+        Query usernamesQuery = dbRef.child("users")
+                .orderByChild("username")
+                .equalTo(text);
+        usernamesQuery.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.getChildrenCount() > 0) {
+                    usernameView.setError("Username taken");
+                } else {
+                    usernameView.setError("Username free", checkmark);
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.w(TAG, "queryUsernames:failure", databaseError.toException());
+            }
+        });
     }
 
     /**
@@ -87,16 +155,6 @@ public class SignUpActivity extends AppCompatActivity
         boolean cancel = false;
         View focusView = null;
 
-        // Check for a valid email
-        if (TextUtils.isEmpty(email)) {
-            emailView.setError("This field is required");
-            focusView = emailView;
-            cancel = true;
-        } else if (!isEmailValid(email)) {
-            emailView.setError("This email address is invalid");
-            focusView = emailView;
-            cancel = true;
-        }
 
         // Check for a valid password
         if (TextUtils.isEmpty(password)) {
@@ -113,9 +171,22 @@ public class SignUpActivity extends AppCompatActivity
             cancel = true;
         }
 
-        // If name is empty use email
+        // Check for a valid email
+        if (TextUtils.isEmpty(email)) {
+            emailView.setError("This field is required");
+            focusView = emailView;
+            cancel = true;
+        } else if (!isEmailValid(email)) {
+            emailView.setError("This email address is invalid");
+            focusView = emailView;
+            cancel = true;
+        }
+
+        // Check name was entered
         if (TextUtils.isEmpty(name)) {
-            name = email.split("@")[0];
+            usernameView.setError("This field is required");
+            focusView = usernameView;
+            cancel = true;
         }
 
         if (cancel) {
@@ -144,7 +215,6 @@ public class SignUpActivity extends AppCompatActivity
                             }
                         }
                     });
-            //mAuth.signInWithEmailAndPassword(email, password);
             // Wait for user to be authenticated or timeout
             long waitTime = new Date().getTime() + 2 * 1000;
             while( authUser == null && (new Date().getTime() < waitTime) ) {
@@ -166,7 +236,7 @@ public class SignUpActivity extends AppCompatActivity
      * @return  true if valid. false otherwise.
      */
     private boolean isEmailValid(String email) {
-        return email.contains("@");
+        return EmailValidator.getInstance().isValid(email);
     }
 
     /**
