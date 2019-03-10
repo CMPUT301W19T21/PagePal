973a8fddc52ed8964be465de5549d6c96567a2e9
ergezinger <noah.ergezinger@gmail.com>
2019-03-10 16:11:08 -0600

    remove dummy class

--- app/src/main/java/ca/team21/pagepal/Book/BookFragment.java
+++ app/src/main/java/ca/team21/pagepal/Book/BookFragment.java
@@ -11,11 +11,8 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import ca.team21.pagepal.R;
-import ca.team21.pagepal.Book.dummy.DummyContent;
-import ca.team21.pagepal.Book.dummy.DummyContent.DummyItem;
 
 import java.util.ArrayList;
-import java.util.List;
 
 /**
  * A fragment representing a list of Items.
