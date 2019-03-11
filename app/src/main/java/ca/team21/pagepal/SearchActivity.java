e63a37b0390b87d9198dda6b8e7b6443768c698b
Brynn Lewis <bclewis@ualberta.ca>
2019-03-11 08:41:33 -0600

    Implemented Search Basics

---
+++ app/src/main/java/ca/team21/pagepal/SearchActivity.java
@@ -0,0 +1,102 @@
+package ca.team21.pagepal;
+import android.app.SearchManager;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+
+import java.awt.font.TextAttribute;
+
+import ca.team21.pagepal.Book.Book;
+import ca.team21.pagepal.Book.BookList;
+
+/*
+TODO: FIX this
+basic check query of
+
+ */
+
+
+public class SearchActivity extends AppCompatActivity  {
+
+    private static final String TAG = "SearchActivity";
+    private String query;
+    private DatabaseReference reference;
+    private BookList bookList;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+
+        // Initialize Database
+
+        reference = FirebaseDatabase.getInstance().getReference();
+
+        //Get intent (aka receive query)
+        Intent intent = getIntent();
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+             final String keyWord = intent.getStringExtra(SearchManager.QUERY);
+            queryBooks(keyWord);
+        }
+
+    }
+
+    public void queryBooks(final String query) {
+
+        final String[] keyWords = query.split("\\s+");
+
+        // Query Firebase
+        Query bookQuery = reference.child("books");
+
+        bookQuery.addListenerForSingleValueEvent(new ValueEventListener() {
+
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()) {
+
+                    for (DataSnapshot data: dataSnapshot.getChildren()) {
+                        String status = data.child("status").getValue(String.class);
+
+                        // Filter by Status
+
+                        if (status == "Available" || status == "Requested") {
+                            Book book = data.getValue(Book.class);
+                            for (String keyWord: keyWords) {
+                                if ((book.getAuthor().toUpperCase()).contains(keyWord.toUpperCase()) ||
+                                        (book.getTitle().toUpperCase()).contains(keyWord.toUpperCase()) ||
+                                        (book.getDescription().toUpperCase()).contains(keyWord.toUpperCase())) {
+                                    bookList.add(book);
+
+                                }
+                            }
+                        }
+                    }
+
+
+                    }
+                }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError){
+                Log.w(TAG, "queryBooks:failure", databaseError.toException());
+            }
+
+        });
+
+
+        }
+    }
+
+
