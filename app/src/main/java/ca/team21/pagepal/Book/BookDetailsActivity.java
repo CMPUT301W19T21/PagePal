1eaac903deb9609dfb3c17655fa5400c75dc8df4
ergezinger <noah.ergezinger@gmail.com>
2019-03-10 22:28:54 -0600

    start of book details

---
+++ app/src/main/java/ca/team21/pagepal/Book/BookDetailsActivity.java
@@ -0,0 +1,17 @@
+package ca.team21.pagepal.Book;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ca.team21.pagepal.R;
+
+public class BookDetailsActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book_details);
+    }
+}
