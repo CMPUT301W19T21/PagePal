bac955093597f900dd86d7676e173519434fa34d
ergezinger <noah.ergezinger@gmail.com>
2019-04-01 15:52:04 -0600

    fixed history item test

--- app/src/test/java/ca/team21/pagepal/HistoryItemTest.java
+++ app/src/test/java/ca/team21/pagepal/HistoryItemTest.java
@@ -13,51 +13,25 @@ import static org.junit.Assert.assertEquals;
  * Tests for the book history list
  */
 
-//public class HistoryItemTest {
-//
-//    private HistoryItem historyItem;
-//    private Book book1;
-//    private Book book2;
-//    private ArrayList<Book> testList;
-//
-//    /**
-//     * Tests adding a book
-//     */
-//    @Test
-//    public void addBookTest() {
-//        historyItem = new HistoryItem();
-//        book1 = new Book();
-//        book2 = new Book();
-//        testList = new ArrayList<>();
-//
-//        assertEquals(testList, historyItem.getHistoryList());
-//
-//        historyItem.addBook(book1);
-//        testList.add(book1);
-//        assertEquals(testList, historyItem.getHistoryList());
-//
-//        historyItem.addBook(book2);
-//        testList.add(book2);
-//        assertEquals(testList, historyItem.getHistoryList());
-//
-//    }
-//
-//    /**
-//     * Tests removing a book
-//     */
-//    @Test void removeBookTest() {
-//        historyItem = new HistoryItem();
-//        book1 = new Book();
-//        book2 = new Book();
-//        testList = new ArrayList<>();
-//
-//        historyItem.addBook(book1);
-//        historyItem.addBook(book2);
-//        historyItem.removeBook(book1);
-//
-//        testList.add(book1);
-//
-//        assertEquals(testList, historyItem.getHistoryList());
-//
-//    }
-//}
+public class HistoryItemTest {
+
+    private HistoryItem historyItem;
+    private ArrayList<Book> testList;
+
+    /**
+     * Tests adding a book
+     */
+    @Test
+    public void addBookTest() {
+        historyItem = new HistoryItem();
+
+        historyItem.setBookHisTitle("The Hobbit");
+        historyItem.setBookHisAuthor("Tolkien");
+        historyItem.setBookHisGenre("Fantasy");
+
+        assertEquals("The Hobbit", historyItem.getBookHisTitle());
+        assertEquals("Tolkien", historyItem.getBookHisAuthor());
+        assertEquals("Fantasy", historyItem.getBookHisGenre());
+
+    }
+}
