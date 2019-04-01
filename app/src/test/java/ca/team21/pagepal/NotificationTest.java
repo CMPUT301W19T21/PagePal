1ecca989664fe4abc9b7dd7de1f7ef02673b2c73
ergezinger <noah.ergezinger@gmail.com>
2019-04-01 15:45:29 -0600

    fixed notification tests

--- app/src/test/java/ca/team21/pagepal/NotificationTest.java
+++ app/src/test/java/ca/team21/pagepal/NotificationTest.java
@@ -3,25 +3,26 @@ package ca.team21.pagepal;
 import org.junit.Test;
 
 import ca.team21.pagepal.models.Notification;
+import ca.team21.pagepal.models.User;
 
 import static org.junit.Assert.assertEquals;
 
 /**
  * Tests the notification class
  */
-//public class NotificationTest {
-//
-//    private Notification notification = new Notification("You have been notified");
-//
-//    /**
-//     * Tests setting the notification message
-//     */
-//    @Test
-//    public void setMessageTest() {
-//        assertEquals("You have been notified", notification.getMessage());
-//
-//        notification.setMessage("User wants your book");
-//
-//        assertEquals("User wants your book", notification.getMessage());
-//    }
-//}
+public class NotificationTest {
+
+    private Notification notification = new Notification("You have been notified", "Noah", "Tanner", "9000712345", "Tanner");
+
+    /**
+     * Tests setting the notification message
+     */
+    @Test
+    public void setMessageTest() {
+        assertEquals("You have been notified", notification.getMessage());
+
+        notification.setMessage("User wants your book");
+
+        assertEquals("User wants your book", notification.getMessage());
+    }
+}
